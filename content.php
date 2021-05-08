51f49cb8c69a43241a8359baecf389fce1e92b6a
thinhcubi <thinhcubi27@gmail.com>
2021-05-08 09:33:39 +0700

    content.php

---
+++ content.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        article {
+            margin-left: 170px;
+            border-left: 2px solid gray;
+            padding: 1em;
+            overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+<h1>LonDon</h1>
+<p>London is the capital city of England. It is the most populous city in the United Kingdom, with a metropolitan area
+    of over 13 million inhabitants.</p>
+<p>London is the capital city of England. It is the most populous city in the United Kingdom, with a metropolitan area
+    of over 13 million inhabitants.</p>
+</body>
+</html>
